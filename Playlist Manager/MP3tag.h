77ed0f1e8708b042545b8d2380f17bcf71d0f5e4
Danyalo <danylolab@gmail.com>
2017-10-04 01:11:40 +0300

    Next try
    Added source code that actually reads mp3 header into TAGdata structure

---
+++ Playlist Manager/MP3tag.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifndef __MP3tag_h
+#define __MP3tag_h
+
+struct TAGdata
+{
+	char tag[3];
+	char title[30];
+	char artist[30];
+	char album[30];
+	char year[4];
+	char comment[30];
+	int genre;
+};
+
+#endif
