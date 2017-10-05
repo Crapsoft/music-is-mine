6103376d24d2352386f3dab692b7e337752714b4
Max <maxym1998@gmail.com>
2017-10-06 00:42:43 +0300

    MP3path function
    Final version

--- Playlist Manager/main.cpp
+++ Playlist Manager/main.cpp
@@ -5,8 +5,9 @@
 #include <vector>
 #include <Windows.h>
 using namespace std;
-vector<string> SearchFolder(string path) 
+vector<string> MP3path(string path) 
 {
+	//Searching for all files in a folder:
 	vector<string> filenames;
 	string directoryPath(path);
 	LPCSTR fpath = (LPCSTR)directoryPath.c_str();
@@ -23,51 +24,54 @@ vector<string> SearchFolder(string path)
 
 		FindClose(hFind);
 	}
-	return filenames;
-}
-vector<string> MP3Path(string path) 
-{
-	vector<string> files = SearchFolder("C:\\Users\\Public\\Music\\*");
+	
+	//toString
+
 	string str;
-	//Remove ... and desktop.ini files
-	for (int i = 0; i < files.size(); i++)
+	//Remove .. and desktop.ini files
+	for (int i = 0; i < filenames.size(); i++)
 	{
-		if (files[i].compare("desktop.ini") == 0 || files[i].size()<3)
+		if (filenames[i].compare("desktop.ini") == 0 || filenames[i].size()<4)
 		{
-			files.erase(files.begin() + i);
-			vector<string>(files).swap(files);
+			filenames.erase(filenames.begin() + i);
+			vector<string>(filenames).swap(filenames);
 		}
 	}
 	//Remove not .mp3 files
-	for (int i = 0; i < files.size(); i++)
+	for (int i = 0; i < filenames.size(); i++)
 	{
-		
-		int size = files[i].size();
-		str = files[i];
-		
-		if (str[size - 1] != '3'|| str[size - 2] !='p' || str[size - 3] != 'm')
+
+		int size = filenames[i].size();
+		str = filenames[i];
+
+		if (str[size - 1] != '3' || str[size - 2] != 'p' || str[size - 3] != 'm')
 		{
-			files.erase(files.begin() + i);
-			vector<string>(files).swap(files);
+			filenames.erase(filenames.begin() + i);
+			vector<string>(filenames).swap(filenames);
 		}
 	}
-	return files;
+	//Adding full address 
+	string path2 = path.substr(0, path.size() - 1);
+	vector<string> v;
+	for (int i = 0; i < filenames.size()-1; i++)
+	{
+		v.push_back(path2 + filenames[i]);
+	}
+	return v;
 }
+//C:\\Users\\Public\\Music\\*    for personal use
+
+
 
 
 int main()
 {
-	/*vector<string> v = SearchFolder("C:\\Users\\Public\\Music\\*");
-	for (int i = 0; i < v.size(); i++) {
-		cout << v[i] << endl;
-	}*/
-	
-	vector<string> v = MP3Path("s");
+	vector<string> v = MP3path("C:\\Users\\Public\\Music\\*");
 	for (int i = 0; i < v.size(); i++)
 	{
 		cout << v[i] << endl;
 	}
-
+	
 	/*TAGdata file("C:\\Users\\Public\\Music\\fd68d237712c49.mp3");
 	cout << file.getArtist() << ' ' << file.getAlbum() << ' ' << file.getTitle() << endl;*/
 	system("pause");
