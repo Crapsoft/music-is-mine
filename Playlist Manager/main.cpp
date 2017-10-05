5503d87604c7bdc38507e9d94d59b5e0ec6bc289
Max <maxym1998@gmail.com>
2017-10-05 23:16:46 +0300

    List of MP3

--- Playlist Manager/main.cpp
+++ Playlist Manager/main.cpp
@@ -25,13 +25,50 @@ vector<string> SearchFolder(string path)
 	}
 	return filenames;
 }
+vector<string> MP3Path(string path) 
+{
+	vector<string> files = SearchFolder("C:\\Users\\Public\\Music\\*");
+	string str;
+	//Remove ... and desktop.ini files
+	for (int i = 0; i < files.size(); i++)
+	{
+		if (files[i].compare("desktop.ini") == 0 || files[i].size()<3)
+		{
+			files.erase(files.begin() + i);
+			vector<string>(files).swap(files);
+		}
+	}
+	//Remove not .mp3 files
+	for (int i = 0; i < files.size(); i++)
+	{
+		
+		int size = files[i].size();
+		str = files[i];
+		
+		if (str[size - 1] != '3'|| str[size - 2] !='p' || str[size - 3] != 'm')
+		{
+			files.erase(files.begin() + i);
+			vector<string>(files).swap(files);
+		}
+	}
+	return files;
+}
 
 
 int main()
 {
-	vector<string> v = SearchFolder("C:\\Users\\Public\\Music\\*");
-	cout<< v.back();
-	/*TAGdata file("Eleven Wives.mp3");
+	/*vector<string> v = SearchFolder("C:\\Users\\Public\\Music\\*");
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << endl;
+	}*/
+	
+	vector<string> v = MP3Path("s");
+	for (int i = 0; i < v.size(); i++)
+	{
+		cout << v[i] << endl;
+	}
+
+	/*TAGdata file("C:\\Users\\Public\\Music\\fd68d237712c49.mp3");
 	cout << file.getArtist() << ' ' << file.getAlbum() << ' ' << file.getTitle() << endl;*/
 	system("pause");
 	return 0;
