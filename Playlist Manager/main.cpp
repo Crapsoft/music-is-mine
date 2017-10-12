35a60008b6592f3482f460f8a7563b0e45dfcb05
prdgma <prdgma@users.noreply.github.com>
2017-10-12 23:09:37 +0300

    upd main

--- Playlist Manager/main.cpp
+++ Playlist Manager/main.cpp
@@ -4,16 +4,31 @@
 #include <string>
 #include <vector>
 #include <Windows.h>
-#include <Playlist.h>
+#include "Playlist.h"
 
 using namespace std;
+
+void ls();
+void pwd();
+void cdfolder(string com);
+void cdback();
 
 //C:\\Users\\Public\\Music\\*    for personal use
 int main()
 {
 
-	/*TAGdata file("C:\\Users\\Public\\Music\\fd68d237712c49.mp3");
-	cout << file.getArtist() << ' ' << file.getAlbum() << ' ' << file.getTitle() << endl;*/
+	TAGdata file("C:\\Users\\Public\\Music\\fd68d237712c49.mp3");
+	//cout << file.getArtist() << ' ' << file.getAlbum() << ' ' << file.getTitle() << endl;
+	string operation = "";
+	do
+	{
+		getline(cin, operation);
+		if (operation.find("ls") == 0)
+		{
+			ls();
+		}
+	} 
+	while (operation != "exit");
 	system("pause");
 	return 0;
 }
